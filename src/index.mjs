f543a05d313798664df16caf94663cff6c66a7be
yousukehasegawa <3312756@mchcgr.com>
2023-10-17 07:37:11 +0000

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -1,10 +1,14 @@
-import "./styles.css";
+let val2 = "let変数";
+console.log(val2);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+val2 = "let変数を上書き";
+console.log(val1);
+
+const val4 = {
+  name: hasey,
+  age: 39,
+};
+
+val4.age = 40;
+
+console.log(hasey);
